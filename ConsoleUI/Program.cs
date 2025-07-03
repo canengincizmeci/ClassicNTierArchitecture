a202272727f13adf9ad294fd079b280ceba7dc9c
Can Engin Çizmeci <canncizmeci@gmail.com>
2025-07-03 21:12:20 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.EfCore;
+using DataAccess.Concrete.InMemory;
+
+Console.WriteLine("Hello, World!");
+
+
+//SOLID 
+//Open Closed Principle :Yaptığın yazılıma yeni bir özellik ekliyosan mevcuttaki hiç bir koduna dokunamazsın sadece entityframewrok klasörü ekledik
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+
+foreach (var product in productManager.GetByUnitPrice(50, 100))
+{
+    Console.WriteLine(product.ProductName);
+}
+
+
+
+
---
+++ DataAccess/Concrete/EfCore/NorthWindContext.cs
@@ -0,0 +1,37 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EfCore
+{
+    //Context : Db tabloları ile proje class'larını bağlamak
+    public class NorthWindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Gerçek projede böyle olur     optionsBuilder.UseSqlServer(@"Server=175.45.2.12");
+
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-CGDTBSJ\MSSQLSERVER5;Database=Northwind_ing;Trusted_Connection=True;");
+
+            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-CGDTBSJ\\MSSQLSERVER5;Database=Northwind_ing;Trusted_Connection=True;");
+            ////optionsBuilder.UseSqlServer(@"Server=DESKTOP-CGDTBSJ;Database=Northwind_ing;Trusted_Connection=True;");
+
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Kategoriler { get; set; }
+
+        public DbSet<Customer> Musteriler { get; set; }
+
+
+
+
+
+
+    }
+}
